a474280f8585ef20b19c247b84cc387c10c4e733
Ari Ferreros <tinytabby66@gmail.com>
2025-04-21 16:26:19 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,13 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - collector game
+// Author:Ari Ferreros <abnferre@ucsc.edu> & Emmet Slater<limslate@ucsc.edu>
+// Date:April 2024
 
-// Constants
+	
+// Initialize board with items on random row/column values
+// initialize character in the middle of the board
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Print the board after every valid key press
+   // Loop through each row
+       // Loop through each column
+           // Print item, character, or space
+		// if no items, print WIN
