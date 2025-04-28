d76fe1969079d0879575286af7b7a2bd6d326b0b
Ari Ferreros <tinytabby66@gmail.com>
2025-04-28 11:20:27 -0700

    Lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,32 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Exploring objects and arrays
+// Author: Ari Ferreros
+// Date:04/28/2025
 
-// Constants
+// means of transport array
+myTransport = ["Bike", "Bus", "Walking"]
+
+// my main ride object
+myMainRide = {
+  make: "Archie",
+  model: "Fang",
+  color: "Dusty White",
+  year: 1997,
+
+  // function to calculate the age
+  age: function () {
+    return 2025 - this.year;
+  }
+}
+
+// output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
 
 // Functions
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
