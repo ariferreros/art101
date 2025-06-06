5170362f03e4d71db601d4afb3068b9ad850e4e1
Ari Ferreros <tinytabby66@gmail.com>
2025-06-05 23:33:52 -0700

    finish

---
+++ lab16/js/lab.js
@@ -0,0 +1,26 @@
+// Ari Ferreros
+// Lab 16 APIs
+// June 5 2025
+
+function comic() {
+  $.ajax({
+    url: 'https://corsproxy.io/?https://xkcd.com/614/info.0.json',
+    dataType: 'json',
+    type: 'GET',
+    success: function (comicObj) {
+      console.log(comicObj)
+      console.log(comicObj.title)
+      const title = comicObj.title
+      const img = comicObj.img
+      const alt = comicObj.alt
+      // const transcript = comicObj.transcript
+      $('#output').append(
+        `<h1>${title}</h1>` +
+          `<img id = "comic" src="${img}" alt="${alt}" />` +
+          // `<p>${transcript}</p>` +
+          `<p>${alt}</p>`
+      )
+    },
+  })
+}
+comic()
